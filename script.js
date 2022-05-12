5d244d34c78bc0bd147837ad5c909776743cb825
placidaxid <nwanagaplacid@gmail.com>
2022-05-12 05:25:53 -0500

    all done

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
     // Player name 
-    var Epsilon = "Player 1"; 
-    var Alpha = "Player 2"; 
+    var player1 = "Player 1"; 
+    var player2 = "Player 2"; 
     // Function to change the player name 
     function editNames() { 
-       Epsilon = prompt("Change Player1 name"); 
-       Alpha = prompt("Change player2 name"); 
+       player1 = prompt("Change Player1 name"); 
+       player2 = prompt("Change player2 name"); 
        document.querySelector("p.Player1").innerHTML = Epsilon; 
        document.querySelector("p.Player2").innerHTML = Alpha; 
 
@@ -12,23 +12,23 @@
     // Function to roll the dice 
     function rollTheDice() { 
         setTimeout(function () { 
-            var randomNumber1 = Math.floor(Math.random() * 6) + 1; 
-            var randomNumber2 = Math.floor(Math.random() * 6) + 1; 
+            var randomNum1 = Math.floor(Math.random() * 6) + 1; 
+            var randomNum2 = Math.floor(Math.random() * 6) + 1; 
             document.querySelector(".img1").setAttribute("src", 
-                "dice" + randomNumber1 + ".png"); 
+                "dice" + randomNum1 + ".png"); 
             document.querySelector(".img2").setAttribute("src", 
-                "dice" + randomNumber2 + ".png"); 
+                "dice" + randomNum2 + ".png"); 
   
-            if (randomNumber1 === randomNumber2) { 
+            if (randomNum1 === randomNum2) { 
                 document.querySelector("h1").innerHTML = "Draw!"; 
             } 
-            else if (randomNumber1 < randomNumber2) { 
+            else if (randomNum1 < randomNum2) { 
                 document.querySelector("h1").innerHTML 
-                                = (Alpha + " WINS!"); 
+                                = (player1 + " WINS!"); 
             } 
             else { 
                 document.querySelector("h1").innerHTML 
-                                = (Epsilon + " WINS!"); 
+                                = (player2 + " WINS!"); 
             } 
         }); 
     } 
